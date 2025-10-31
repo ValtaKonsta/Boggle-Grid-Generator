ee4f474621661b674e094ec452dc0f3e4d46b9ee
ValtaKonsta <valta.konsta@gmail.com>
2025-10-31 15:47:43 +0200

    -Added constants.py - Created grid generation function - Created simple visualizatin of a created grid - Added UI for choosing grid language and size

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 #Copy-pasted example app.py for Streamlit app
 import streamlit as st
-from gridgenerator.core import generate_grid
+from bogglegridgenerator.core import generate_grid
+from bogglegridgenerator.core import minimal_letter_grid
+from constants import SUPPORTED_LANGUAGES
 
 st.set_page_config(page_title="GridGenerator", layout="wide")
 
 st.title("🧩 Grid Generator Tool")
 
-rows = st.number_input("Rows", min_value=1, value=5)
-cols = st.number_input("Columns", min_value=1, value=5)
+selectedLanguage = st.selectbox("Choose a language", list(SUPPORTED_LANGUAGES.keys()))
+selectedSize = st.slider("Select the dimensions of your grid (4x4 etc.", min_value=2, max_value=10, value=4)
+customWeights = st.toggle("Use custom weights")
 generate_button = st.button("Generate Grid")
 
 if generate_button:
-    grid = generate_grid(rows, cols)
-    st.write(grid)
+    grid = generate_grid(selectedLanguage, selectedSize)
+    minimal_letter_grid(grid)
--- bogglegridgenerator/core.py
+++ bogglegridgenerator/core.py
@@ -1,7 +1,27 @@
 #Copy-pasted example app logic
-
+import random
 import numpy as np
+from constants import SUPPORTED_LANGUAGES
+import streamlit as st
 
-def generate_grid(rows, cols):
-    """Generate a simple numeric grid."""
-    return np.arange(rows * cols).reshape(rows, cols)
+
+def generate_grid(language_name: str, grid_size: int = 4):
+    lang_data = SUPPORTED_LANGUAGES[language_name]
+    letters = lang_data["letters"]
+    weights = lang_data["weights"]
+
+    grid_letters = random.choices(letters, weights, k = grid_size * grid_size)
+    return [grid_letters[i : i+grid_size] for i in range(0, len(grid_letters), grid_size)]
+
+
+
+
+def minimal_letter_grid(grid):
+    html = "<table style='border-collapse: separate; border-spacing: 5px;'>"
+    for row in grid:
+        html += "<tr>"
+        for cell in row:
+            html += f"<td style='padding: 20px; text-align: center; font-size: 28px; font-weight: bold; width: 80px;'>{cell}</td>"
+        html += "</tr>"
+    html += "</table>"
+    st.markdown(html, unsafe_allow_html=True)
